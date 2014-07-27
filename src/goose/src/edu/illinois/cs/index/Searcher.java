d37e2686cef30269f07935a891b6c0de0c1961e5
Larry Resnik <lsresni2@illinois.edu>
2014-07-26 22:33:53 -0500

    Added Goose's files to the project. Not as a submodule.

---
+++ src/goose/src/edu/illinois/cs/index/Searcher.java
@@ -0,0 +1,161 @@
+package edu.illinois.cs.index;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class Searcher
+{
+    private IndexSearcher indexSearcher;
+    private SpecialAnalyzer analyzer;
+    private static SimpleHTMLFormatter formatter;
+    private static final int numFragments = 4;
+    private static final String defaultField = "content";
+
+    /**
+     * Sets up the Lucene index Searcher with the specified index.
+     *
+     * @param indexPath
+     *            The path to the desired Lucene index.
+     */
+    public Searcher(String indexPath)
+    {
+        try
+        {
+            IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(indexPath)));
+            indexSearcher = new IndexSearcher(reader);
+            indexSearcher.setSimilarity(new BM25Similarity());     // BM25Similarity exists in Lucene
+            //indexSearcher.setSimilarity(new TFIDFSimilarity());  // this is our custom similarity function
+            //indexSearcher.setSimilarity(new SimpleSimilarity()); // simple boolean occurrence example function
+            analyzer = new SpecialAnalyzer();
+            formatter = new SimpleHTMLFormatter("<strong>", "</strong>");
+        }
+        catch(IOException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * The main search function.
+     * @param searchQuery Set this object's attributes as needed.
+     * @return
+     */
+    public SearchResult search(SearchQuery searchQuery)
+    {
+        BooleanQuery combinedQuery = new BooleanQuery();
+        for(String field: searchQuery.fields())
+        {
+            QueryParser parser = new QueryParser(Version.LUCENE_46, field, analyzer);
+            try
+            {
+                Query textQuery = parser.parse(searchQuery.queryText());
+                combinedQuery.add(textQuery, BooleanClause.Occur.MUST);
+            }
+            catch(ParseException exception)
+            {
+                exception.printStackTrace();
+            }
+        }
+
+        return runSearch(combinedQuery, searchQuery);
+    }
+
+    /**
+     * The simplest search function. Searches the abstract field and returns a
+     * the default number of results.
+     *
+     * @param queryText
+     *            The text to search
+     * @return the SearchResult
+     */
+    public SearchResult search(String queryText)
+    {
+        return search(new SearchQuery(queryText, defaultField));
+    }
+
+    /**
+     * Performs the actual Lucene search.
+     *
+     * @param luceneQuery
+     * @param numResults
+     * @return the SearchResult
+     */
+    private SearchResult runSearch(Query luceneQuery, SearchQuery searchQuery)
+    {
+        try
+        {
+            TopDocs docs = indexSearcher.search(luceneQuery, searchQuery.fromDoc() + searchQuery.numResults());
+            ScoreDoc[] hits = docs.scoreDocs;
+            String field = searchQuery.fields().get(0);
+
+            SearchResult searchResult = new SearchResult(searchQuery, docs.totalHits);
+            for(ScoreDoc hit : hits)
+            {
+                Document doc = indexSearcher.doc(hit.doc);
+                ResultDoc rdoc = new ResultDoc(hit.doc);
+
+                String highlighted = null;
+                try
+                {
+                    Highlighter highlighter = new Highlighter(formatter, new QueryScorer(luceneQuery));
+                    String title = doc.getField("title").stringValue();
+                    rdoc.title(title);
+                    String contents = doc.getField(field).stringValue();
+                    rdoc.content(contents);
+                    String url = doc.getField("url").stringValue();
+                    rdoc.url(url);
+                    String[] snippets = highlighter.getBestFragments(analyzer, field, contents, numFragments);
+                    highlighted = createOneSnippet(snippets);
+                }
+                catch(InvalidTokenOffsetsException exception)
+                {
+                    exception.printStackTrace();
+                    highlighted = "(no snippets yet)";
+                }
+
+                searchResult.addResult(rdoc);
+                searchResult.setSnippet(rdoc, highlighted);
+            }
+
+            searchResult.trimResults(searchQuery.fromDoc());
+            return searchResult;
+        }
+        catch(IOException exception)
+        {
+            exception.printStackTrace();
+        }
+        return new SearchResult(searchQuery);
+    }
+
+    /**
+     * Create one string of all the extracted snippets from the highlighter
+     * @param snippets
+     * @return
+     */
+    private String createOneSnippet(String[] snippets)
+    {
+        String result = " ... ";
+        for(String s: snippets)
+            result += s + " ... ";
+        return result;
+    }
+}
